c2003537b9a5a3591fec67ca817ffc8433bac65f
enxtiu <kuznetsov.fedo@yandex.ru>
2024-12-06 20:29:41 +0700

    add users_handl

--- handlers/users_handl.py
+++ handlers/users_handl.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+import logging
 
+from aiogram import filters, types, F, Router
+from aiogram.filters import CommandStart
+
+logging.basicConfig(level=logging.DEBUG, format='#{levelname} {name}: {lineno} - {funcName} - {message}', style='{')
+logger = logging.getLogger(__name__)
+
+router = Router()
+
+@router.message(filters.CommandStart)
+async def get_start(message: types.Message) -> None:
+    pass
 
 if __name__ == '__main__':
     pass
